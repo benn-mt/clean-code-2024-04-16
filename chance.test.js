344a9f312b0e3f94410b1226fda28cb80aba94ed
Ben Noble <ben.noble@madetech.com>
2024-04-16 15:15:33 +0100

    extracted fifty fifty chance

--- chance.test.js
+++ chance.test.js
@@ -1,14 +1,16 @@
 const {Chance} = require("./chance");
 
+const FIFTY_FIFTY = new Chance(0.5);
+
 describe ('Chance objects....', () => {
     test('can be compared for equality', () => {
-        expect(new Chance(0.5).equals(new Chance(0.5))).toBe(true);
+        expect(FIFTY_FIFTY.equals(FIFTY_FIFTY)).toBe(true);
         expect(new Chance(0.6).equals(new Chance(0.6))).toBe(true);
         expect(new Chance(0.3).equals(new Chance(0.7))).toBe(false);
         expect(new Chance(0.7).equals(new Chance(0.3))).toBe(false);
     });
 
     test('can be inverted', () => {
-        expect(new Chance(0.5).not().equals(new Chance(0.5))).toBe(true);
+        expect(FIFTY_FIFTY.not().equals(FIFTY_FIFTY)).toBe(true);
     })
 });
