c981a16764d134488d6bc3c76edd0821bfa2e7fd
Ben Noble <ben.noble@madetech.com>
2024-04-16 15:02:48 +0100

    Better message for test

--- chance.test.js
+++ chance.test.js
@@ -1,7 +1,7 @@
 const {Chance} = require("./chance");
 
 describe ('Chance objects....', () => {
-    test('are equal when likelihood matches', () => {
+    test('can be compared for equality', () => {
         expect(new Chance(0.5).equals(new Chance(0.5))).toBe(true);
     });
 });
