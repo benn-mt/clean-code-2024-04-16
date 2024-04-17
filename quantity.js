f18e8112d93678f2ca73f056089fcdcd5b7ecf84
Ben Noble <ben.noble@madetech.com>
2024-04-17 11:24:49 +0100

    Quantity compares units in equality

--- quantity.js
+++ quantity.js
@@ -1,13 +1,16 @@
 class Quantity{
     constructor(amount, unit){
         this._amount = amount;
+        this._unit = unit;
     }
 
     equals(other){
-        return this._amount == other._amount;
+        return this._amount == other._amount
+        && this._unit == other._unit;
     }
 }
 
 const TEASPOON = new Object();
+const TABLESPOON = new Object();
 
-module.exports = {Quantity, TEASPOON}
+module.exports = {Quantity, TEASPOON, TABLESPOON}
--- quantity.test.js
+++ quantity.test.js
@@ -1,9 +1,10 @@
-const {Quantity, TEASPOON} = require("./quantity");
+const {Quantity, TEASPOON, TABLESPOON} = require("./quantity");
 
 describe ('Measurements....', () => {
     test('can be compared for equality', () => {
         expect(new Quantity(1, TEASPOON).equals(new Quantity(1, TEASPOON))).toBe(true);
         expect(new Quantity(1, TEASPOON).equals(new Quantity(2, TEASPOON))).toBe(false);
         expect(new Quantity(2, TEASPOON).equals(new Quantity(1, TEASPOON))).toBe(false);
+        expect(new Quantity(1, TEASPOON).equals(new Quantity(1, TABLESPOON))).toBe(false);
     });
 });
