71c043fa3999b791f45aa20c40b942e76730f62a
Ben Noble <ben.noble@madetech.com>
2024-04-17 14:30:29 +0100

    Seperated out volume tests

--- quantity.test.js
+++ quantity.test.js
@@ -10,13 +10,4 @@ describe ('Measurements....', () => {
         expect(new Quantity(3, TEASPOON).equals(new Quantity(1, TABLESPOON))).toBe(true);
         expect(new Quantity(2, TABLESPOON).equals(new Quantity(6, TEASPOON))).toBe(true);
     });
-
-    test('Board conversions', () => {
-        expect(TABLESPOON.s(1).equals(TEASPOON.s(3))).toBe(true);
-        expect(OUNCE.s(1).equals(TABLESPOON.s(2))).toBe(true);
-        expect(CUP.s(1).equals(OUNCE.s(8))).toBe(true);
-        expect(PINT.s(1).equals(CUP.s(2))).toBe(true);
-        expect(QUART.s(1).equals(PINT.s(2))).toBe(true);
-        expect(GALLON.s(1).equals(QUART.s(4))).toBe(true);
-    });
 });
