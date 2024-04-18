6869e7ea3cc7c79300d757783312d6716e40d580
Ben Noble <ben.noble@madetech.com>
2024-04-18 19:54:15 +0100

    Differentiating RatioQuantity from IntervalQuantity

--- ratio_quantity.js
+++ ratio_quantity.js
@@ -1,26 +1,11 @@
-class RatioQuantity{
-    constructor(amount, unit){
-        this._amount = amount;
-        this._unit = unit;
-    }
+const { IntervalQuantity } = require("./interval_quantity");
 
-    equals(other){
-        return this._unitsAreCompatible(other) &&
-               this._amountsInBaseUnitAreEqual(other);
-    }
+class RatioQuantity extends IntervalQuantity{
+
 
     add(other){
         let totalAmount = this._amount + this._unit.amountInThisUnit(other._amount, other._unit)
         return new RatioQuantity(totalAmount, this._unit);
-    }
-
-    _amountsInBaseUnitAreEqual(other) {
-        return this._amount ==
-            this._unit.amountInThisUnit(other._amount, other._unit);
-    }
-
-    _unitsAreCompatible(other) {
-        return this._unit.isCompatibleWith(other._unit);
     }
 }
 
