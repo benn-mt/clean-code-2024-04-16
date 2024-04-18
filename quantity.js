59767711763df1af06e53457ec1ea547667aa687
Ben Noble <ben.noble@madetech.com>
2024-04-18 10:56:24 +0100

    Quantity uses unit.amountinthisunit

--- quantity.js
+++ quantity.js
@@ -13,8 +13,8 @@ class Quantity{
         if (!this._unit.isCompatibleWith(other._unit)){
             throw new TypeError("Incompatible Units");
         }
-        return new Quantity(this._unit.amountInBaseUnit(this._amount) +
-            other._unit.amountInBaseUnit(other._amount), this._unit._baseUnit);
+        return new Quantity(this._amount +
+            this._unit.amountInThisUnit(other._amount, other._unit), this._unit);
     }
 
     _amountsInBaseUnitAreEqual(other) {
