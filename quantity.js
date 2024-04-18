6168149cc6d3a61cc90e05e7c9dd4979c85665b2
Ben Noble <ben.noble@madetech.com>
2024-04-18 11:39:07 +0100

    Removed unused/duplicated code

--- quantity.js
+++ quantity.js
@@ -10,9 +10,6 @@ class Quantity{
     }
 
     add(other){
-        if (!this._unit.isCompatibleWith(other._unit)){
-            throw new TypeError("Incompatible Units");
-        }
         return new Quantity(this._amount +
             this._unit.amountInThisUnit(other._amount, other._unit), this._unit);
     }
